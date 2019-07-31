e3de3f86a361e62a6e91fc3787f330f1c3dd7e1c
ateet <16ucs050@lnmiit.ac.in>
2019-07-31 20:01:29 +0530

    js starting, initializing variables

---
+++ comfy-home project/app.js
@@ -0,0 +1,11 @@
+//variables
+
+const cartBtn = document.querySelector(".cart-btn");
+const closeCartBtn = document.querySelector(".close-cart");
+const clearCartBtn = document.querySelector(".clear-cart");
+const cartDom = document.querySelector(".cart");
+const clearOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const productsDom = document.querySelector(".products-center");
